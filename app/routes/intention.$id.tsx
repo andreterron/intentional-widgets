721cadeab3fc4712e82008640b94b2bb29d67b45
Andre Terron <git@terron.me>
2025-06-21 13:59:45 -0700

    Improve intention page

--- app/routes/intention.$id.tsx
+++ app/routes/intention.$id.tsx
@@ -2,6 +2,9 @@ import { ClientLoaderFunctionArgs } from "react-router";
 import { Route } from "./+types/intention.$id";
 import { Intention } from "../lib/data/intention";
 
+import React, { useState } from "react";
+import { Input } from "../components/ui/input";
+
 export function meta({ params }: Route.MetaArgs) {
   const title = Intention.model.selectById(params.id).get()?.title;
   return [
@@ -27,6 +30,27 @@ export async function clientLoader({ params }: ClientLoaderFunctionArgs) {
 export default function IntentionIdRoute({
   loaderData: { intention },
 }: Route.ComponentProps) {
-  // No updates for now
-  return <div>{intention?.title ?? "No title"}</div>;
+  const [title, setTitle] = useState(intention?.title ?? "No title");
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setTitle(e.target.value);
+
+  return (
+    <div className="min-h-screen w-full bg-white">
+      <div className="py-5 px-4 max-w-2xl">
+        <input
+          value={title}
+          onChange={handleTitleChange}
+          aria-label="Page title"
+          className="px-2 py-1.5 -mx-2 text-2xl w-full box-content mb-6"
+        />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <Input type="text" placeholder="Generate" className="text-base" />
+        </form>
+      </div>
+    </div>
+  );
 }
